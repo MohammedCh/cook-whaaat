01bd5abc2f784c520cebad983a3d3f0d8ffa9282
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2022-02-10 15:29:13 +0100

    favorites, myrecipes, delete & edit functionality

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 require("./config/session.config")(app);
 require("./config")(app);
 
 const projectName = "cook-whaaat";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
@@ -27,5 +27,29 @@ app.use("/", index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+// Handlebars helper that checks for if v1 is NOT equal to v2
+hbs.handlebars.registerHelper("ifNotEqual", function (v1, v2, options) {
+  if (v1 != v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+// Handlebars helper that checks for if v1 is equal to v2
+hbs.handlebars.registerHelper("ifEqual", function (v1, v2, options) {
+  if (v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+// Handlebars helper that checks for if v1 is equal to v2
+hbs.handlebars.registerHelper("ifContains", function (array, v2, options) {
+  if (array.indexOf(v2.toString()) >= 0) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
 
 module.exports = app;
