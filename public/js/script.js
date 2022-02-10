db08f7bb9f45ef4c78031336f5ceb6c27b16483b
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2022-02-10 19:45:50 +0100

    moved jquery code from hbs to public/js/script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,23 @@ document.addEventListener(
   },
   false
 );
+
+// script to change add to favorites button
+$("#favoriteButton").click(function () {
+  $("#favoriteButton").html("Added to favorites!");
+  $("#favoriteButton").css("background-color", "transparent");
+  $("#favoriteButton").css("color", "black");
+  $("#favoriteButton").css("border-style", "none");
+});
+
+// script that loads the searchResults partial within this page by calling POST "recipes/search"
+$("#search-button").click(function () {
+  $.post("recipes/search", $("#searchInput").serialize(), function (data) {
+    $("#searchResults").html(data);
+  });
+});
+
+// script to change add to favorites icon button
+$(".material-icons").click(function () {
+  $("#" + this.id).addClass("favoriteIconToggled");
+});
